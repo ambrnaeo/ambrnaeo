56c28c2f20f5401c3f5e1d7ed8a6b6779241864c
anthony <anthony@unknown-f0-b4-79-15-7a-d8.config>
2011-02-10 20:56:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ambrnaeo::Application.config.secret_token = '8994ee0354558c7f701c0183c45994d09f183f6b5058da7fd15bec411be3d50a593a2a321ee2199c09e72e6cab41f0e1178c5e4cc8ce19ad47cc2f53e8a22660'
